2ff0cfbd9dd7aebcc04275a2d811dad9eda7df42
prclaudioraposo <claudio.raposo@anchieta.br>
2024-10-07 11:01:49 -0300

    add

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,16 @@
 "use strict";
 const message = "Hello Word";
 console.log(message);
+const idade = [33, 18, 20, 18];
+const cadastro = [
+    { nome: "Claudio", idade: 33 },
+    { nome: "José", idade: 93 }
+];
+console.log(cadastro);
+var Color;
+(function (Color) {
+    Color["Red"] = "Vermelho";
+    Color["Green"] = "Verde";
+    Color["Blue"] = "Azul";
+})(Color || (Color = {}));
+console.log(Color.Green);
--- index.ts
+++ index.ts
@@ -1,3 +1,25 @@
 const message: string = "Hello Word";
 
 console.log(message)
+
+const idade: Array<number> = [33, 18, 20, 18];
+
+interface Pessoa {
+    nome: string,
+    idade: number,
+}
+
+const cadastro: Pessoa[] = [
+    { nome: "Claudio", idade: 33 },
+    { nome: "José", idade: 93 }
+];
+
+console.log(cadastro)
+
+enum Color {
+    Red = "Vermelho",
+    Green = "Verde",
+    Blue = "Azul"
+}
+
+console.log(Color.Green)
